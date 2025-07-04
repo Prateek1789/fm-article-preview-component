c405bde79824b8d22902e18b100160fb86ed8d80
PrateekP <prateekpanta1789@gmail.com>
2025-07-04 23:34:01 +0530

    Added image object that returns all the images

---
+++ src/assets/images/index.js
@@ -0,0 +1,17 @@
+import drawer from './drawers.jpg'
+import avatar from './avatar-michelle.jpg'
+import share from './icon-share.svg'
+import facebook from './icon-facebook.svg'
+import twitter from './icon-twitter.svg'
+import pinterest from './icon-pinterest.svg'
+
+const images = {
+    drawer,
+    avatar,
+    share,
+    facebook,
+    twitter,
+    pinterest
+}
+
+export default images
