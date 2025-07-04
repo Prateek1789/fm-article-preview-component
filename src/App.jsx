9893c9c78e38d4d600b955fb45a6e48b25796baf
PrateekP <prateekpanta1789@gmail.com>
2025-07-04 23:35:12 +0530

    Added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import Card from './components/Card'
+import Card from './components/card'
 
 function App() {
-
   return (
-    <>
+    <main className="w-full h-screen p-4 flex justify-center items-center">
       <Card />
-    </>
+    </main>
   )
 }
 
